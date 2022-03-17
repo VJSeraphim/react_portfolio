6d7358c964bf74d6c4bed9fb5f8a7b7d422429c6
VJS <minsung_96@naver.com>
2022-03-17 23:54:05 +0900

    File Structures

---
+++ frontend/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+import { About, Header, Footer, Skills, Testimonial, Works } from './container'
+import { Navbar } from './components'
+
+const App = () => {
+    return (
+        <div className="app">
+            <Navbar />
+            <Header />
+            <About />
+            <Works />
+            <Skills />
+            <Testimonial />
+            <Footer />
+        </div>
+    )
+}
+
+export default App
