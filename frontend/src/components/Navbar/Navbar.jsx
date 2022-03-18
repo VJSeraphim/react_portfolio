dcf794c32198b248ffd362f9578de3df0690abd7
VJS <minsung_96@naver.com>
2022-03-18 23:57:58 +0900

    scss init

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,10 +1,28 @@
 import React from 'react'
 
 import './Navbar.scss'
+import { images } from '../../constants'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav className="app__navbar">
+      <div className="app__navbar-logo">
+        <img 
+          src={images.logo}
+          alt="logo"
+        />
+      </div>
+      <ul className="app__navbar-links">
+        {['home', 'about', 'works', 'skills', 'contact'].map((i) => (
+          <li key={`link=${i}`} className="app__flex p-text">
+            <div />
+            <a href={`#${i}`}>
+              {i}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </nav>
   )
 }
 
