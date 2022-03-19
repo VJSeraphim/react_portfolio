86c98d1a7aa2f54e48f80e1269c74626c3b34b58
VJS <minsung_96@naver.com>
2022-03-19 23:59:19 +0900

    NAvbar #2

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import './Navbar.scss'
 import { images } from '../../constants'
+import { HiMenuAlt4, HiX } from 'react-icons/Hi'
+import { motion } from 'framer-motion'
 
 const Navbar = () => {
+  const [toggle, setToggle] = useState(false)
+
   return (
     <nav className="app__navbar">
       <div className="app__navbar-logo">
@@ -22,6 +26,31 @@ const Navbar = () => {
           </li>
         ))}
       </ul>
+
+      <div className="app__navbar-menu">
+        <HiMenuAlt4 
+          onClick={() => setToggle(true)}
+        />
+        {
+          toggle && (
+            <motion.div 
+              whileInView={{ x:[300, 0] }} 
+              transition={{ duration: 0.85, ease: 'easeOut'}}
+            >
+              <HiX 
+                onClick={() => setToggle(false)}
+              />
+              {['home', 'about', 'works', 'skills', 'contact'].map((i) => (
+                <li key={i}>
+                  <a href={`#${i}`} onClick={() => setToggle(false)}>
+                    {i}
+                  </a>
+                </li>
+              ))}
+            </motion.div>
+          )
+        }
+      </div>
     </nav>
   )
 }
