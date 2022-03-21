447a4d927952dd6da8a861e968dd70133b78a0ec
VJS <minsung_96@naver.com>
2022-03-21 23:59:21 +0900

    Header #2

--- frontend/src/container/Header/Header.jsx
+++ frontend/src/container/Header/Header.jsx
@@ -9,7 +9,7 @@ const Header = () => {
     <div className="app__header app__flex">
       <motion.div
         whileInView={{ x: [-100, 0], opacity: [0, 1]}}
-        transition={{ duration: 0.5}}
+        transition={{ duration: 0.5 }}
         className="app__header-info"
       >
         <div className="app__header-badge">
@@ -17,8 +17,35 @@ const Header = () => {
             <span>
               
             </span>
+            <div style={{ marginLeft: 20 }}>
+              <p className="p-text">
+                Hello, I am
+              </p>
+              <h1 className="head-text">
+                VJS
+              </h1>
+            </div>
+          </div>
+
+          <div className="tag-cmp app_Flex">
+            <p className="p-text">
+              Junior Frontend Developer
+            </p>
           </div>
         </div>
+      </motion.div>
+      <motion.div
+        whileInView={{ opacity: [0, 1]}}
+        transition={{ duration: 0.5, delayChildren: 0.5 }}
+        className="app__header-img"
+      >
+        <img 
+          src={images.profile}
+          alt="profile-bg"
+        />
+      </motion.div>
+      <motion.div>
+        
       </motion.div>
     </div>
   )
