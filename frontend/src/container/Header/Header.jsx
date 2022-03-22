09717add9ca9d3e91bf6e87eab32268103e68dbf
VJS <minsung_96@naver.com>
2022-03-22 23:58:17 +0900

    Header #3

--- frontend/src/container/Header/Header.jsx
+++ frontend/src/container/Header/Header.jsx
@@ -4,9 +4,20 @@ import { images } from '../../constants'
 
 import './Header.scss'
 
+const scaleVariants = {
+  whileInView: {
+    scale: [0, 1],
+    opacity: [0, 1],
+    transition: {
+      duration: 1,
+      ease: 'easeInOut'
+    }
+  }
+}
+
 const Header = () => {
   return (
-    <div className="app__header app__flex">
+    <div id="home" className="app__header app__flex">
       <motion.div
         whileInView={{ x: [-100, 0], opacity: [0, 1]}}
         transition={{ duration: 0.5 }}
@@ -43,9 +54,27 @@ const Header = () => {
           src={images.profile}
           alt="profile-bg"
         />
+        <motion.img
+          whileInView={{ scale: [0, 1]}}
+          transition={{ duration: 1, ease: 'easeInOut' }}
+          src={images.circle}
+          alt="profile_circle"  
+          className="overlay_circle"      
+        />
       </motion.div>
-      <motion.div>
-        
+      <motion.div
+        variant={scaleVariants}
+        whileInView={scaleVariants.whileInView}
+        className="app__header-circles"
+      >
+        {[images.flutter, images.redux, images.sass].map((circle, i)=> (
+          <div className="circle-cmp app__flex" key={`circle-${i}`}>
+            <img 
+              src={circle}
+              alt="circle"
+            />
+          </div>
+        ))}
       </motion.div>
     </div>
   )
